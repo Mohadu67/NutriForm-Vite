5582419c48877b4d070e5a7f7b63b7d33ea1072a
mohadu67 <mohadu67@users.noreply.github.com>
2025-09-08 08:54:15 +0200

    Maj exo

--- frontend/src/components/Auth/HistoryUser/HistoryHeader.jsx
+++ frontend/src/components/Auth/HistoryUser/HistoryHeader.jsx
@@ -7,7 +7,7 @@ export default function HistoryHeader({ displayName }) {
     <header className={style["popup-header-row"]}>
       <h2 className={style.headerTitle}>Historique</h2>
       {displayName && (
-        <p className={style.headerSubtitle}>Salut <span>{displayName}</span>👋, voici ton tableau de bord : tu y trouveras toutes les informations nécessaires pour atteindre ton objectif.</p>
+        <p className={style.headerSubtitle}>Salut <span>{displayName}</span>👋 Voici ton tableau de bord, ton QG pour écraser tes objectifs</p>
       )}
     </header>
   );
