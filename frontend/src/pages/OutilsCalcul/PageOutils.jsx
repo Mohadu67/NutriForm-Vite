f51c4991e16f587d98cbe71b5f87b98ee359a59d
mohadu67 <mohadu67@users.noreply.github.com>
2025-09-28 03:54:02 +0200

    modifl style pub

--- frontend/src/pages/OutilsCalcul/PageOutils.jsx
+++ frontend/src/pages/OutilsCalcul/PageOutils.jsx
@@ -2,15 +2,16 @@ import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import OutilsCalcul from "./OutilsCalcul.jsx";
 import AdSlot from "../../components/AdSlot/AdSlot.jsx";
+import styles from "./OutilsCalcul.module.css";
 
 export default function PageOutils() {
   return (
     <>
       <Header />
       <OutilsCalcul />
-      <div style={{ margin: "20px auto", maxWidth: "300px", borderRadius: "10px", textAlign: "center" }}>
-        <AdSlot slot="1234567890" />
-      </div>
+        <div className={styles.adContainer}>
+          <AdSlot slot="1234567890" />
+        </div>
       <Footer />
     </>
   );
