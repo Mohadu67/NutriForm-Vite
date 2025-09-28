720762a264b0113d24b95da7cf1cf1a4f6e66c46
mohadu67 <mohadu67@users.noreply.github.com>
2025-09-28 03:32:46 +0200

    Modif chemin dossier adslot

--- frontend/src/pages/OutilsCalcul/PageOutils.jsx
+++ frontend/src/pages/OutilsCalcul/PageOutils.jsx
@@ -1,14 +1,14 @@
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import OutilsCalcul from "./OutilsCalcul.jsx";
-import AdSlot from "../../components/Ads/AdSlot.jsx";
+import AdSlot from "../../components/AdSlot/AdSlot.jsx";
 
 export default function PageOutils() {
   return (
     <>
       <Header />
       <OutilsCalcul />
-      <div style={{ margin: "20px auto", maxWidth: "300px", textAlign: "center" }}>
+      <div style={{ margin: "20px auto", maxWidth: "300px", borderRadius: "10px", textAlign: "center" }}>
         <AdSlot slot="1234567890" />
       </div>
       <Footer />
