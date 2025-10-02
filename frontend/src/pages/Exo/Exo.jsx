824bcd78226462fd626abbb088aa9ef849211f4f
mohadu67 <mohadu67@users.noreply.github.com>
2025-10-02 16:48:54 +0200

    delete AdSlot

--- frontend/src/pages/Exo/Exo.jsx
+++ frontend/src/pages/Exo/Exo.jsx
@@ -2,8 +2,6 @@ import usePageTitle from "../../hooks/usePageTitle.js";
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import FormExo from "../../components/Exercice/FormExo/FormExo.jsx";
-import AdSlot from "../../components/AdSlot/AdSlot.jsx";
-
 
 export default function ExoPage () {
   usePageTitle("S'entraîner");
@@ -12,7 +10,6 @@ export default function ExoPage () {
     <>
       <Header />
       <main>
-        <AdSlot slot="1234567890" />
         <FormExo />
       </main>
       <Footer />
--- frontend/src/pages/OutilsCalcul/PageOutils.jsx
+++ frontend/src/pages/OutilsCalcul/PageOutils.jsx
@@ -1,13 +1,11 @@
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import OutilsCalcul from "./OutilsCalcul.jsx";
-import AdSlot from "../../components/AdSlot/AdSlot.jsx";
 
 export default function PageOutils() {
   return (
     <>
       <Header />
-        <AdSlot slot="1234567890" />
       <OutilsCalcul />
       <Footer />
     </>
